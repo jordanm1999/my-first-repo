4c605c972a41f264dd110bfe66f7fc11dc1a4788
Jordan Mockford <jordanmockford@gmail.com>
2026-09-17 07:58:00 -0600

    made changes

--- ifelse.py.py
+++
@@ -1,9 +0,0 @@
- # do the exercise on brightspace about generating a number from 1-100
-
-# module 3 lesson 4 activites section on brightspace 
-# ask the user to generate a number between 1-100
-
-import random
-random_number = random.randint(1, 100)
-print(f"Generate a random number: {random_number}")
-
